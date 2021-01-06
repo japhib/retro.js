606ca2d9176f17441007d239be27fc1c4ffb43e9
japhib <jphbergeson@gmail.com>
2021-01-05 19:30:53 -0700

    add dev server option

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     entry: './src/main.ts',
+    devServer: {
+        contentBase: './dist',
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
@@ -16,5 +20,8 @@ module.exports = {
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
             { test: /\.ts$/, use: ["ts-loader"], exclude: /node_modules/ },
         ],
-    }
+    },
+    plugins: [
+        new CopyPlugin({patterns: ['index.html']})
+    ],
 };
